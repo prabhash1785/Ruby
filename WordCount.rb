21dfab7795b02ec931fce8a5bd39503dab4d35c3
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-03 00:04:49 -0700

    Program to count frequency of words in a sentence

---
+++ WordCount.rb
@@ -0,0 +1,14 @@
+def find_frequency(sentence, word)
+  count = 0
+  listOfWords = sentence.split(" ")
+  for w in listOfWords
+    if w.upcase == word.upcase then
+      count = count + 1
+     end
+    # count = count + 1
+    # puts count
+  end
+  return count
+end
+
+puts find_frequency("Love this love this hello this", "love")
